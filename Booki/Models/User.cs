60d6fa81b1112b71144372dd7fc326d6a33827ed
jalvadev <josealvarezvivar@gmail.com>
2024-01-10 21:36:05 +0100

    Added basic model classes.

---
+++ Booki/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Booki.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsVerified { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+
+        public Bookshelf Bookshelf { get; set; }
+    }
+}
