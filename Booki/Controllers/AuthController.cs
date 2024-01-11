88d8783ef666ed4c7e1a9813aa6bd18767111f8f
jalvadev <josealvarezvivar@gmail.com>
2024-01-11 19:33:36 +0100

    Removed unnecesary controllers and added AuthController.

---
+++ Booki/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Booki.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booki.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        public AuthController() { }
+
+        public IActionResult Login(UserLoginDTO user)
+        {
+            return Ok(user);
+        }
+    }
+}
