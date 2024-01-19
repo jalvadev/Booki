f934be69801f897569ba47e5d829570370e2fccf
jalvadev <josealvarezvivar@gmail.com>
2024-01-19 18:39:07 +0100

    Adding new class to control image saving.

---
+++ Booki/Helpers/ImageHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace Booki.Helpers
+{
+    public static class ImageHelper
+    {
+        public static byte[] ConvertBase64OnBytes(string base64Image)
+        {
+            byte[] imageByter = Convert.FromBase64String(base64Image);
+
+            return imageByter;
+        }
+
+        public static string CreateUserDirectoryIfNotExists(string username)
+        {
+            string fullPathDirectory = $"/{username}";
+
+            if(!Directory.Exists(fullPathDirectory))
+                Directory.CreateDirectory(fullPathDirectory);
+
+        
+            return fullPathDirectory;
+        }
+
+        public static bool SaveImage(string fileName, byte[] image)
+        {
+            File.WriteAllBytes(fileName, image);
+
+            return true;
+        }
+    }
+}
