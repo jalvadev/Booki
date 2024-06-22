a08b160b173c2876154e3533327f1d21c74e2368
jalvadev <josealvarezvivar@gmail.com>
2024-06-22 18:39:55 +0200

    Changed CoverPictureName to nullable.

--- Booki/Models/DTOs/BookDTO.cs
+++ Booki/Models/DTOs/BookDTO.cs
@@ -6,7 +6,7 @@
 
         public string CoverPicture { get; set; }
 
-        public string CoverPictureName { get; set; }
+        public string? CoverPictureName { get; set; }
 
         public string Title { get; set; }
 
